93280c5890a84487fc5e4fd126a7c1e88964d8ed
gnjyothi <gnjyothiece@gmail.com>
2020-03-23 11:37:56 -0400

    adding source file LoginTest.java

---
+++ src/test/java/com/assignment/opencart/LoginTest.java
@@ -0,0 +1,43 @@
+package com.opencart.assignment;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTest {
+	
+	public void testLogin(String browser)	{
+		String url = "http://localhost/opencart/admin/";
+		WebDriver driver = null;
+		
+		//launch browser
+		if(browser.equals("chrome"))	{
+			System.setProperty("webdriver.chrome.driver","c://chromedriver.exe");
+			driver = new ChromeDriver();
+		}	else if(browser.equals("firefox"))	{
+			System.setProperty("webdriver.gecko.driver", "c://geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get(url);
+		
+		driver.findElement(By.id("input-username")).sendKeys("sample@xyz.com");
+		driver.findElement(By.id("input-password")).sendKeys("sample123");
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		
+		String message = driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).getText();
+		System.out.println(message);
+		
+		driver.quit();
+	}
+	
+	public static void main(String[] args)	{
+		LoginTest login = new LoginTest();
+		login.testLogin("firefox");
+	}
+
+}
